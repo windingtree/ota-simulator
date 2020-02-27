9cb7f3081f89b6b46ca35f2eda51dac57cb40749
Mathieu TAHON <mathieu.tahon@noath.net>
2020-02-27 17:10:36 +0100

    Small fix to have the full log of the request

--- api/index.js
+++ api/index.js
@@ -4,9 +4,24 @@ var path = require('path');
 const filePath = path.join(__dirname, 'OTA_HotelResNotifRS-Commit.xml');
 
 module.exports = (req, res) => {
-    console.log(req.body);
-    fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data) {
-        res.setHeader('Content-Type', 'text/html');
-        res.status(200).send(data);
+    let body = [];
+    
+    // WHen HTTP Data is received, we push it to the body
+    req.on('data', (chunk) => {
+        body.push(chunk);
     });
+
+    // Once we have everything, we answer
+    req.on('end', (chunk) => {
+        // Extract the body
+        body = Buffer.concat(body).toString();
+        console.log(body);
+        
+        // Send the answer
+        fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data) {
+            res.setHeader('Content-Type', 'text/html');
+            res.status(200).send(data);
+        });
+    });
+
 };
