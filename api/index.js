a0554a3041304a013d4300358ac074b0fc1cd5c7
Mathieu TAHON <mathieu.tahon@noath.net>
2020-03-04 08:33:02 +0100

    Added support for OTA Availability

--- api/index.js
+++ api/index.js
@@ -1,13 +1,19 @@
 var fs = require('fs');
 var path = require('path');
+const url = require('url');
 
-const filePath = path.join(__dirname, 'OTA_HotelResNotifRS-Commit.xml');
+const resNotif = path.join(__dirname, 'OTA_HotelResNotifRS-Commit.xml');
+const avail = path.join(__dirname, 'OTA_HotelAvailRS.xml');
 
 module.exports = (req, res) => {
     let body = [];
     
     // WHen HTTP Data is received, we push it to the body
     req.on('data', (chunk) => {
+        if(body.length == 0) {
+            res.setHeader('Content-Type', 'text/xml;charset=UTF-8');
+            res.status(200);
+        }
         body.push(chunk);
     });
 
@@ -16,11 +22,18 @@ module.exports = (req, res) => {
         // Extract the body
         body = Buffer.concat(body).toString();
         console.log(body);
+
+        // Determine which file to serve
+        const queryObject = url.parse(req.url,true).query;
+        let filePath = resNotif;
+        if(queryObject.ota !== undefined) {
+            filePath = (queryObject.ota == 'getOTAHotelAvailability' ? avail : resNotif);
+        }
         
         // Send the answer
         fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data) {
-            res.setHeader('Content-Type', 'text/html');
-            res.status(200).send(data);
+            res.setHeader('Content-Length', data.length);
+            res.end(data);
         });
     });
 
