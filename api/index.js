85ef20b1523a7d89866cb440a9def66768119648
tomashq <tomashq@gmail.com>
2020-09-25 18:18:25 +0200

    added booking cancellation dummy response

--- api/index.js
+++ api/index.js
@@ -2,15 +2,19 @@ var fs = require('fs');
 var path = require('path');
 const url = require('url');
 
-const resNotif = path.join(__dirname, 'OTA_HotelResNotifRS-Commit.xml');
-const avail = path.join(__dirname, 'OTA_HotelAvailRS.xml');
+const responses = {
+    'getOTAHotelRes': path.join(__dirname, 'OTA_HotelResNotifRS-Commit.xml'),
+    'cancel': path.join(__dirname, 'OTA_HotelResNotifRS-Cancel.xml'),
+    'getOTAHotelAvailability': path.join(__dirname, 'OTA_HotelAvailRS.xml')
+}
+const DEFAULT = 'getOTAHotelRes';
 
 module.exports = (req, res) => {
     let body = [];
-    
+
     // WHen HTTP Data is received, we push it to the body
     req.on('data', (chunk) => {
-        if(body.length == 0) {
+        if (body.length === 0) {
             res.setHeader('Content-Type', 'text/xml;charset=UTF-8');
             res.status(200);
         }
@@ -24,14 +28,15 @@ module.exports = (req, res) => {
         console.log(body);
 
         // Determine which file to serve
-        const queryObject = url.parse(req.url,true).query;
-        let filePath = resNotif;
-        if(queryObject.ota !== undefined) {
-            filePath = (queryObject.ota == 'getOTAHotelAvailability' ? avail : resNotif);
+        const queryObject = url.parse(req.url, true).query;
+        let filePath;
+        if (queryObject.ota !== undefined) {
+            filePath = responses[queryObject.ota];
         }
-        
+        if (!filePath)
+            filePath = responses[DEFAULT];
         // Send the answer
-        fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data) {
+        fs.readFile(filePath, {encoding: 'utf-8'}, function (err, data) {
             res.setHeader('Content-Length', data.length);
             res.end(data);
         });
